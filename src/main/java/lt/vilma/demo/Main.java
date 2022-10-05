f80a1ff855568639f01923a97bffdc12153ef3a5
Vartotojas <Vilmaboa@gmail.com>
2022-10-05 15:44:38 +0300

    Initialise sayHelloLt from TestA in main()

--- src/main/java/lt/vilma/demo/Main.java
+++ src/main/java/lt/vilma/demo/Main.java
@@ -3,7 +3,7 @@ package lt.vilma.demo;
 public class Main {
 
     public static  void main(String[]args){
-
+    TestA.sayHelloLt();
         System.out.println("Hello world");
     }
 }
--- src/main/java/lt/vilma/demo/TestA.java
+++ src/main/java/lt/vilma/demo/TestA.java
@@ -1,11 +1,8 @@
 package lt.vilma.demo;
 
-
-
-
 public class TestA {
 
-    public void sayHelloLt() {
+    public static void sayHelloLt() {
         System.out.println("Labas pasauli");
     }
 }
